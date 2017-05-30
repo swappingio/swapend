845e69c21f7b5d436cd277efe3876b512af01dd8
Bendik E.Dyrli <skandix@datapor.no>
2017-05-30 20:47:58 +0200

    fixed so the path to the config is 'dynamic' rather than a static path.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,28 +1,39 @@
 package config
 
 import (
-	"log"
-
 	"github.com/pajlada/kkonfig"
+	"log"
+	"os/user"
 )
 
 type Specification struct {
 	Database struct {
+		Hostname string `json:"Hostname"`
 		Username string `json:"Username"`
 		Password string `json:"Password"`
-		Hostname string `json:"Hostname"`
 		Dbname   string `json:"Dbname"`
 	} `json:"Database"`
 	Mailgun struct {
+		Enabled      bool   `json:"Enabled"`
 		APIKey       string `json:"APIKey"`
 		Domain       string `json:"Domain"`
 		PublicAPIKey string `json:"PublicAPIKey"`
 		URL          string `json:"URL"`
 	} `json:"Mailgun"`
-	Authentication struct {
+	Sessions struct {
 		CookieSecret string `json:"CookieSecret"`
 		StorageName  string `json:"StorageName"`
 	} `json:"Authentication"`
+	Redis struct {
+		Hostname string `json:"Hostname"`
+		Port     int    `json:"Port"`
+		Timeout  int    `json:"Timeout"`
+	} `json:"Redis"`
+	Transcoder struct {
+		ConcurrentTranscodes int  `json:"ConcurrentTranscodes"`
+		Threads              int  `json:"Threads"`
+		Debug                bool `json:"Debug"`
+	} `json:"Transcoder"`
 }
 
 var (
@@ -31,12 +42,23 @@ var (
 
 func init() {
 
+	usr, hpath := user.Current()
+	if hpath != nil {
+		log.Fatal(hpath)
+	}
+
 	var test []string
-	test = append(test, "/home/coral/.streamcred.json")
+	test = append(test, usr.HomeDir+"/.streamcred.json")
 	err := kkonfig.Process("web", test, &s)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	//Standard Values
+	s.Transcoder.ConcurrentTranscodes = 1
+	s.Transcoder.Threads = 1
+	s.Transcoder.Debug = true
+
 	log.Println("Loaded Config.")
 }
 
