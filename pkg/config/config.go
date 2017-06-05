095252696e66c0468be0c4f4b474893f46c0c822
skandix <skandix@datapor.no>
2017-06-05 21:29:10 +0200

    checking for config file
    added checking if file exisist that it should print found config rather
    than always saying config loaded when it didn't load shit...

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,7 +3,8 @@ package config
 import (
 	"github.com/pajlada/kkonfig"
 	"log"
-	"os/user"
+	"os"
+	"runtime"
 )
 
 type Specification struct {
@@ -42,24 +43,54 @@ var (
 
 func init() {
 
+	var pathSeperator string
+
+	// will clean this up later, but for now it's okay 
+	if runtime.GOOS == "windows"{
+		pathSeperator = "\\"
+
+	} else if runtime.GOOS == "darwin"{
+		pathSeperator = "/"
+
+	} else if runtime.GOOS == "linux" {
+		pathSeperator = "/"
+
+	} else {
+		log.Printf("THIS IS UNACEPTABLE !!")
+		os.Exit(0) //something happend that wasn't supposed to happend...
+	}
+
+	var configFile string
+	configFile = "/.streamcred.json"
+
 	usr, hpath := user.Current()
 	if hpath != nil {
 		log.Fatal(hpath)
 	}
 
 	var test []string
-	test = append(test, usr.HomeDir+"/.streamcred.json")
+	test = append(test, usr.HomeDir+configFile)
 	err := kkonfig.Process("web", test, &s)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+
+	_, fileErr := os.Stat(PATH)
+	if os.IsNotExist(fileErr){
+		log.Printf("The config '%s' doesn't exist.\n", configFile)
+		os.Exit(127) // bash exit code for problems with path
+	} else {
+		log.Printf("\nFound Config. \n%s", PATH)
+	}
+
 
 	//Standard Values
 	s.Transcoder.ConcurrentTranscodes = 1
 	s.Transcoder.Threads = 1
 	s.Transcoder.Debug = true
 
-	log.Println("Loaded Config.")
+	//log.Println("Loaded Config.")
 }
 
 func GetConfig() Specification {
